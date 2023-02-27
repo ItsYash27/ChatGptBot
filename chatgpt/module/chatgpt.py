25a9f3d7cd88ca2afe60dab735b49ee9b573b592
Pirated <71497650+ItsYash27@users.noreply.github.com>
2023-02-27 19:52:26 +0530

    Update chatgpt.py

--- chatgpt/module/chatgpt.py
+++ chatgpt/module/chatgpt.py
@@ -47,7 +47,7 @@ async def chatgpt(c: Client, m: Message):
         "top_p": 0.3,
         "frequency_penalty": 0.5,
     }
-    ran = await m.reply("ʟᴏᴏᴋɪɴɢ ғᴏʀ ʀᴇsᴜʟᴛs/ɴ ⭐ᴡʜɪʟᴇ ᴡᴀɪᴛɪɴɢ ᴇᴀᴛ ғɪᴠᴇ sᴛᴀʀ ᴀɴᴅ ᴅᴏ ɴᴏᴛʜɪɴɢ...", quote=True)
+    ran = await m.reply("ʟᴏᴏᴋɪɴɢ ғᴏʀ ʀᴇsᴜʟᴛs/n ⭐ᴡʜɪʟᴇ ᴡᴀɪᴛɪɴɢ ᴇᴀᴛ ғɪᴠᴇ sᴛᴀʀ ᴀɴᴅ ᴅᴏ ɴᴏᴛʜɪɴɢ...", quote=True)
     try:
         response = (await http.post("https://api.openai.com/v1/completions", headers=headers, json=json_data)).json()
         await ran.edit(response["choices"][0]["text"])
